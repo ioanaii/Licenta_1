61e12710b9a6db851419ba25764cfe97afb77e27
ioanaii <ioana.ionescu650@gmail.com>
2023-05-17 19:54:11 +0200

    ---

--- src/main/java/Pages/ItineraryPage.java
+++ src/main/java/Pages/ItineraryPage.java
@@ -3,6 +3,8 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 import java.util.List;
 
@@ -11,7 +13,7 @@ public class ItineraryPage {
 
 
     WebDriver driver;
-    By radio_bookedFlight_Itinerary = By.xpath("//input[@type='radio' and @value='out']");
+    By radio_bookedFlight_Itinerary = By.xpath("//input[@type='checkbox' and @value='on']");
     By button_removeSelected_Itinerary = By.name("removeSelected");
     By button_removeAll_Itinerary = By.name("removeAll");
 
@@ -24,22 +26,38 @@ public class ItineraryPage {
 
     //to do: fix this
     public void removeSelected() {
-
-        //TO DO: get checked items by 'name' and verify that removed items are not available after submissions
         List<WebElement> bookedFlights = driver.findElements(radio_bookedFlight_Itinerary);
-
         int bookedFlightsCount = bookedFlights.size();
         int checkedItemsCount = 0;
-        System.out.println("checked items count:"+ bookedFlightsCount);
 
-        for (int i = 0; i < bookedFlightsCount; i += 2) {
-            bookedFlights.get(i).click();
-            checkedItemsCount++;
+        System.out.println("bookedFlightsCount: " + bookedFlightsCount);
+
+        for (int i = 0; i < bookedFlightsCount; i++) {
+            if (i % 2 == 0) {  // Select every second radio button
+                bookedFlights.get(i).click();
+                checkedItemsCount++;
+            }
         }
 
-        //System.out.println("checked items count:"+ checkedItemsCount);
+        System.out.println("Checked items count after selection: " + checkedItemsCount);
 
         driver.findElement(button_removeSelected_Itinerary).click();
+
+        String currentURL = driver.getCurrentUrl();
+        String expectedURL = "http://localhost:8080/mtours/servlet/com.mercurytours.servlet.IteneraryServlet?procSub=1";
+        Assert.assertEquals(currentURL, expectedURL, "User is directed to the wrong page");
+    }
+
+    public void removeAll(){
+        SoftAssert softAssert = new SoftAssert();
+        driver.findElement(button_removeAll_Itinerary).click();
+
+        String currentURL = driver.getCurrentUrl();
+        String expectedURL = "http://localhost:8080/mtours/servlet/com.mercurytours.servlet.IteneraryServlet?procSub=1";
+        Assert.assertEquals(currentURL, expectedURL, "User is directed to the wrong page");
+        softAssert.assertTrue(driver.getPageSource().contains("Note: There are no orders for this account"), "Text not found");
     }
 
 }
+
+
--- src/test/java/FlightBookingTests.java
+++ src/test/java/FlightBookingTests.java
@@ -58,8 +58,8 @@ public class FlightBookingTests {
         flightFinder.enterFlightDetails("roundtrip", "2", "Frankfurt", "Feb", "22", "Acapulco", "Feb", "22", "Coach", "No Preference");
         flightSelect.selectFlights(1,2);
         flightSelect.successfulFlightSelect();
-        flightPurchase.inputFlightPurchase("", "Doe", "1123");
-        flightPurchase.inputFlightPurchase("John", "", "1123");
+        //flightPurchase.inputFlightPurchase("", "Doe", "1123");
+        //flightPurchase.inputFlightPurchase("John", "", "1123");
         flightPurchase.inputFlightPurchase("John", "Dose", "1123");
 
         homePage.accessItinerary(driver);
